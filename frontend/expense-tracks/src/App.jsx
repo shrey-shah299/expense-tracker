5c0fbcbde443f3e873728d9e25946da4f6892670
shrey <shreyshah026@gmail.com>
2026-03-05 16:34:58 +0530

    Added vercel ananalytics view

--- frontend/expense-tracks/src/App.jsx
+++ frontend/expense-tracks/src/App.jsx
@@ -8,7 +8,7 @@ import { AuthProvider } from './AuthContext.jsx';
 import ProtectedRoute from './components/ProtectedRoute.jsx';
 import NotFoundError from './pages/NotFoundError.jsx';
 import Login from './pages/Login.jsx';
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 import { Routes, Route, useLocation, Navigate } from 'react-router-dom';
 import './App.css'
 
