3f3c1c6880e2a73a3494abf1445ad6a1b13e4f26
shrey <shreyshah026@gmail.com>
2026-03-05 16:45:50 +0530

    Updated

--- frontend/expense-tracks/src/App.jsx
+++ frontend/expense-tracks/src/App.jsx
@@ -8,7 +8,7 @@ import { AuthProvider } from './AuthContext.jsx';
 import ProtectedRoute from './components/ProtectedRoute.jsx';
 import NotFoundError from './pages/NotFoundError.jsx';
 import Login from './pages/Login.jsx';
-import { Analytics } from "@vercel/analytics/react"
+
 import { Routes, Route, useLocation, Navigate } from 'react-router-dom';
 import './App.css'
 
@@ -33,7 +33,6 @@ function App() {
 
           <Route path="*" element={<Navigate to="/404" replace />} />
         </Routes>
-        <Analytics />
       </div>
     </>
   )
