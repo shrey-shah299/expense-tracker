8b39e5d94bd60868d60270d00e1a27681fe709a4
shrey <shreyshah026@gmail.com>
2026-05-06 18:31:41 +0530

    Updated api.js

--- frontend/expense-tracks/src/api.js
+++ frontend/expense-tracks/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const API_BASE = import.meta.env.VITE_API_URL || "https://expense-tracker-i45m.onrender.com/api";
+const API_BASE = "https://expense-tracker-i45m.onrender.com/api";
 //import.meta.env.VITE_API_URL || "https://expense-tracker-i45m.onrender.com/api";
 
 const api = axios.create({
